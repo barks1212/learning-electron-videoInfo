0c370c4ecbbc358aa8a3a97affce16c06802dd64
barks1212 <willbarkerwork@gmail.com>
2018-01-20 15:22:25 +0000

    added functionality for browser window to listen to electron process, window now displays video length

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const electron = require('electron');
 const { app, BrowserWindow, ipcMain } = electron;
 const ffmpeg = require('fluent-ffmpeg');
 
+let mainWindow;
+
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 ipcMain.on('videoSubmit', (event, path) => { //tells electron to listen for information the browser window is sending
-  ffmpeg.ffprobe(path, (err, metadata) => {
 
+  ffmpeg.ffprobe(path, (err, metadata) => { //grabs video metadata object
+    
+    mainWindow.webContents.send('videoMetadata', metadata.format.duration) //sends back specified metadata to browser window
   })
 })
