dfa3475d6b405598f3984f1f0e7e522575272746
barks1212 <willbarkerwork@gmail.com>
2018-01-20 15:05:53 +0000

    set up electron process to listen for variables being sent

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const electron = require('electron');
 const { app, BrowserWindow, ipcMain } = electron;
+const ffmpeg = require('fluent-ffmpeg');
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-ipcMain.on('videoSubmit', () => {
-  
+ipcMain.on('videoSubmit', (event, path) => { //tells electron to listen for information the browser window is sending
+  ffmpeg.ffprobe(path, (err, metadata) => {
+
+  })
 })
